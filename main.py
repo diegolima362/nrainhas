9b8324e8359eff33b2b9a4bb06e747e7e4d2e6b8
trolleg <renangustavoo101@gmail.com>
2021-05-07 11:46:03 -0300

    usuario informa n rainhas

--- main.py
+++ main.py
@@ -6,40 +6,45 @@ from tabulate import tabulate
 
 import genetic
 
-queens = 8
-target_value = (queens * (queens - 1)) / 2
+queens = int(input('Digite o numero n de rainhas: '))
 
-population_size = 50
-generation_limit = 100
-single_answer = True
-survivals_per_gen = 2
+if queens >= 4 and queens <= 25:
+    target_value = (queens * (queens - 1)) / 2
 
-iterations = 1
+    population_size = 50
+    generation_limit = 100
+    single_answer = True
+    survivals_per_gen = 2
 
-plot_data = []
+    iterations = 1
 
-for i in range(iterations):
-    fitness = partial(genetic.fitness)
+    plot_data = []
 
-    start = time.process_time()
+    for i in range(iterations):
+        fitness = partial(genetic.fitness)
 
-    population, generations, data = genetic.run_evolution(
-        population_size=population_size,
-        queens_total=queens,
-        generation_limit=generation_limit,
-        survivals=survivals_per_gen,
-        single=single_answer
-    )
-    end = time.time()
+        start = time.process_time()
 
-    plot_data = data
+        population, generations, data = genetic.run_evolution(
+            population_size=population_size,
+            queens_total=queens,
+            generation_limit=generation_limit,
+            survivals=survivals_per_gen,
+            single=single_answer
+        )
+        end = time.time()
 
-    print(f'Iteração: {i} | Gerações: {generations} |', end=' ')
-    print(f'Tempo: {time.process_time() - start}s')
-    print(f'Melhor gene: {population[0]}')
-    print(f'Precisão: {(fitness(population[0]) / target_value * 100):.3f} %')
-    genetic.show_board(population[0])
-    print('- - - - - - - - - - - - - - - - -\n')
+        plot_data = data
 
-labels = ['Geração', 'Encontrou Solução', 'Melhor Gene', 'Pontos', 'Precisão']
-print(tabulate(plot_data, headers=labels, tablefmt='fancy_grid'))
+        print(f'Iteração: {i} | Gerações: {generations} |', end=' ')
+        print(f'Tempo: {time.process_time() - start}s')
+        print(f'Melhor gene: {population[0]}')
+        print(f'Precisão: {(fitness(population[0]) / target_value * 100):.3f} %')
+        genetic.show_board(population[0])
+        print('- - - - - - - - - - - - - - - - -\n')
+
+    labels = ['Geração', 'Encontrou Solução', 'Melhor Gene', 'Pontos', 'Precisão']
+    print(tabulate(plot_data, headers=labels, tablefmt='fancy_grid'))
+
+else:
+  print('\nOpcao Invalida.')
